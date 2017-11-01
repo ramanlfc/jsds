d0d952c4347cdefdd8ebab950b137680223110ce
raman <ramanlfc@gmail.com>
2017-11-01 19:11:14 +0530

    added stack implementation

---
+++ data-structures/stack/stack-test.js
@@ -0,0 +1,59 @@
+describe('Stack test cases', () => {
+
+    it('should add X elements to stack', () => {
+
+        let s = new Stack();
+
+        for (let i = 0; i < 5; i++)
+            s.push(i);
+
+        expect(s.count).toBe(5);
+    });
+
+    it('should pop X elements from stack', () => {
+
+        let s = new Stack();
+
+        for (let i = 0; i < 5; i++)
+            s.push(i);
+
+        for (let i = 0; i < 2; i++)
+            s.pop();
+
+        expect(s.count).toBe(3);
+    });
+
+    it('should return top element without removing it', () => {
+
+        let s = new Stack();
+
+        for (let i = 0; i < 5; i++)
+            s.push(i);
+
+        expect(s.peek()).toBe(4);
+    });
+
+    it('should not allow pop on empty stack', () => {
+
+        let s = new Stack();
+
+        expect(() => {
+            s.pop();
+        }).toThrowError(Error);
+
+    });
+
+    it('should not allow push on full stack', () => {
+
+        let s = new Stack();
+
+        for (let i = 0; i < 10; i++)
+            s.push(i);
+
+        expect(() => {
+            s.push(50);
+        }).toThrowError(Error);
+
+    });
+    
+});
---
+++ data-structures/stack/stack.js
@@ -0,0 +1,59 @@
+class Stack {
+
+    constructor(size) {
+
+        if (size === undefined)
+            size = 10;
+
+        this.data = [];
+        this.size = size;
+        this.top = -1;
+        this.count = 0;
+    }
+
+    isEmpty() {
+        return this.top === -1;
+    }
+
+    isFull() {
+        return this.count == this.size;
+    }
+
+    push(val) {
+
+        if (this.isFull())
+            throw new Error('Stack is full');
+
+        this.data[++this.top] = val;
+        this.count++;
+    }
+
+    pop() {
+
+        if (this.isEmpty())
+            throw new Error('Stack is empty');
+
+        let temp = this.data[this.top--];
+        this.count--;
+        return temp;
+    }
+
+    clear(){
+        this.data= [];
+        this.top = -1;
+        this.count = 0;
+    }
+
+    peek() {
+
+        if (this.isEmpty())
+            throw new Error('Stack is empty');
+
+        return this.data[this.top];
+    }
+
+    display() {
+        console.log(this.data);
+    }
+
+}
